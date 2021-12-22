eff1477a40a05a795f0759a1d7038798d6e7fa06
doandv0510 <60165305+doandv0510@users.noreply.github.com>
2021-12-22 12:31:11 +0700

    update & delete todo

--- controllers/todo.js
+++ controllers/todo.js
@@ -2,7 +2,6 @@ const Todo = require("../models/todo");
 
 exports.createTodo=(req,res)=>{
     const todo = new Todo(req.body);
-
     todo.save((err, task) => {
         if (err || !task) {
             return res.status(400).json({ error: "something went wrong" });
@@ -12,7 +11,9 @@ exports.createTodo=(req,res)=>{
 }
 
 exports.getAllTodos=(req,res)=>{
-    Todo.find().sort("createdAt").exec((err, todos)=>{
+    Todo.find()
+    .sort("createdAt")
+    .exec((err, todos)=>{
         if(err || !todos){
             return res.status(400).json(
                 {
@@ -21,6 +22,52 @@ exports.getAllTodos=(req,res)=>{
         }
         else{
             res.json(todos);
+            // next();
         }
     });
-}
+};
+
+exports.GetById=(req,res,next,todoId)=>{
+    Todo.findById(todoId).exec((err, todo) => {
+        if (err || !todo) {
+            return res.status(400).json({ error: "404 todo not found" });
+        }
+        req.todo = todo;
+        console.log(req.todo);
+        next();
+    });
+};
+
+exports.GetTodo = (req, res) => {
+    return res.json(req.todo);
+};
+
+exports.updateTodo = (req, res) => {
+    const todo = req.todo;
+
+    todo.task = req.body.task;
+
+    todo.save((err, task) => {
+        if (err || !task) {
+            return res
+                .status(400)
+                .json({ error: "something went wrong while updating" });
+        }
+        res.json({ task });
+    });
+};
+
+exports.DeleteTodo = (req, res) => {
+    const todo = req.todo;
+
+    todo.remove((err, task) => {
+        if (err || !task) {
+            return res
+                .status(400)
+                .json({ error: "something went wrong while deteting the todo" });
+        }
+        res.json({
+            message: "Todo deleted successfully!",
+        });
+    });
+};
--- routes/todo.js
+++ routes/todo.js
@@ -4,12 +4,24 @@ const router = express.Router();
 
 const {
     getAllTodos, 
-    createTodo
+    createTodo,
+    GetById,
+    GetTodo,
+    updateTodo,
+    DeleteTodo
 } = require('../controllers/todo'); 
 
 const strLink = "/Todo/";
 router.get(strLink , getAllTodos); 
 
+router.param("todoId", GetById); 
+
 router.post(strLink + "Create", createTodo); 
+
+router.get(strLink + "GetById/:todoId", GetTodo); 
+
+router.put("/todo/:todoId/update", updateTodo);
+
+router.delete("/todo/delete/:todoId", DeleteTodo); 
 
 module.exports = router;
